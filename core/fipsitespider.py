b9f517d3c5e620170950d5cb3a9774e70f4133c4
CJ <2771004336@qq.com>
2024-02-27 11:20:33 +0800

    1

--- core/fipsitespider.py
+++ core/fipsitespider.py
@@ -1,104 +1,16 @@
 from core.basespider import BaseSpider
-from db import db
 
 
-class FipSiteManager:
-    pass
-
-
-class BossSite(BaseSpider):
+class FipSiteManager(BaseSpider):
 
     def __init__(self):
-        super().__init__(connect_over_cdp="http://localhost:9999", headless=False)
-        self.setting = {
-            "url": "https://www.zhipin.com/suzhou/",
-            "job_title": ["python"],
-            "on_task_page": 5
-        }
-        self.msg = f"""
-尊敬{{company_name}}公司{{hr}}您好：
-我是廖先生，一个富有创造力和想法的技术人员，专注于DevOps、后端开发和产品设计。我非常欣赏贵公司的发展和成就，希望能够加入贵公司成为一名员工或者合作伙伴。
-我在DevOps工具开发、后端开发和产品设计领域拥有丰富的经验和深厚的专业知识。我对技术创新充满热情，并且善于解决问题和提供创造性解决方案。
-目前，我正积极寻找新的职业发展机会，对贵公司的工作机会非常感兴趣。我是一位快速学习和适应新环境的人，具备良好的团队合作能力和沟通能力。我相信我的技术和创造力将为贵公司带来巨大的价值。
-如果可能的话，我希望能有机会与贵公司进行面试，详细讨论我如何为贵公司的发展做出贡献。我非常乐意与您分享更多关于我的技能和经验，以及我对技术创新的热情。
-我目前处于在职寻找机会的状态，可以随时安排面试。期待能够得到贵公司的回复，共同探讨未来的合作机会。
-如需了解更多关于我的项目和经历，请联系我以获取我的简历。
-谢谢您的时间和考虑，诚挚期待您的回复！❤
+        super().__init__()
+        self._ctx = None
+        self.page = None
 
-今日已招呼{{count}}条信息
-"""
+    def _init_site(self, **kwargs):
+        if self._ctx is None:
+            self._init_browser(**kwargs)
 
-    @staticmethod
-    def insert(data):
-        db.session.add(data)
-        db.session.commit()
-        db.session.close()
-
-    @staticmethod
-    def check_exist(company_name):
-        return bool(db.session.query(db.Boss).filter_by(company_name=company_name).first())
-
-    def goto_index_page(self):
-        self.open(self.setting["url"])
-
-    def search(self, key):
-        self.fill_element("//input[@class='ipt-search']", key)
-        self.click_element("//button[@class='btn btn-search']")
-
-    def next_page(self):
-        self.click_element("(//div[@class='options-pages']//a/i)[2]")
-
-    def run(self):
-        self.goto_index_page()
-        count = 0
-        row = 0
-
-        for key in self.setting["job_title"]:
-            self.search(key)
-            self.wait_for_timeout(5)
-
-            for _ in range(self.setting["on_task_page"]):
-                self.wait_for_timeout(3)
-                job_list_elements = self.find_elements("//li[@class='job-card-wrapper']")
-                for i in range(1, len(job_list_elements) + 1):
-                    el = self.find_element(f"(//li[@class='job-card-wrapper'])[{i}]")
-                    company_name = self.element_query(el, "//h3/a").text_content()
-                    job_name = self.element_query(el, "//span[@class='job-name']").text_content()
-                    salary = self.element_query(el, "//span[@class='salary']").text_content()
-                    hrname = self.element_query(el, "//div[@class='info-public']").text_content()
-                    hrposition = self.element_query(el, "//div[@class='info-public']/em").text_content()
-                    address = self.element_query(el, "//span[@class='job-area']").text_content()
-                    hr = hrname
-                    row += 1
-                    # print(company_name, job_name, salary, address, hr)
-                    print(f"{company_name:10s}{job_name:10s}{salary:12s}{address:10s}{hr:10s}")
-
-                    if self.check_exist(company_name) is True:
-                        continue
-
-                    self.insert(db.Boss(company_name=company_name,
-                                        job_name=job_name,
-                                        salary=salary,
-                                        address=address,
-                                        category=key))
-
-                    self.click_element(f"(//div[@class='info-public'])[{row}]")
-                    self.wait_for_timeout(3)
-                    # self.click_element("//div[@contenteditable='true']")
-                    count += 1
-
-                    self.fill_element(
-                        "//div[@contenteditable='true']",
-                        self.msg.format_map({
-                            "company_name": company_name,
-                            "hr": hr,
-                            "count": count
-                        })
-                    )
-                    self.press_key("Enter")
-                    self.wait_for_timeout(1)
-                    self.go_back()
-                    self.wait_for_timeout(3)
-
-                self.next_page()
-                row = 0
+        self._ctx = self._browser.contexts[0]
+        self.page = self._ctx.new_page()
--- core_cop/fipsitespider.py
+++ core_cop/fipsitespider.py
@@ -1,35 +1,23 @@
-import os
 from typing import Optional, Any
 
-from playwright.sync_api import sync_playwright, ElementHandle
+from playwright.sync_api import ElementHandle
+
+from core_cop.basespider import BaseSpider
 
 
-class BaseSpider:
+class FipSiteSpider(BaseSpider):
 
-    def __init__(self, connect_over_cdp=None):
-        """Initialize the spider with optional browser launch arguments."""
-        self._connect_over_cdp = connect_over_cdp
-        self._playwright = sync_playwright()
-        self._driver = self._playwright.start()
-        self._browser = None
-        self._page = None
-        # self.ctx = self.browser.contexts[0]
-        # self.page = self.ctx.new_page()
+    def __init__(self, connect_over_cdp):
+        super().__init__(connect_over_cdp)
+        self._ctx = None
+        self.page = None
 
-    def __enter__(self):
-        """Enter the runtime context related to this object."""
-        return self
+    def init_site(self, **kwargs):
+        if self._ctx is None:
+            self._init_browser(**kwargs)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """Exit the runtime context related to this object."""
-        self.close()
-
-    def _init_browser(self, **kwargs):
-        if self._browser is None:
-            if self._connect_over_cdp:
-                self._browser = self._driver.chromium.connect_over_cdp(self._connect_over_cdp)
-            else:
-                self._browser = self._driver.chromium.launch(**kwargs)
+        self._ctx = self._browser.contexts[0]
+        self.page = self._ctx.new_page()
 
     @staticmethod
     def element_query(element, selector) -> ElementHandle:
@@ -40,7 +28,7 @@ class BaseSpider:
         # self.page = self.browser.new_page()
         self.page.goto(url)
 
-    def perform_action_on_element(self, selector, action):
+    def execute_action(self, selector, action):
         """Find an element by its selector and perform the given action on it."""
         element = self.find_element(selector)
         action(element)
@@ -60,13 +48,13 @@ class BaseSpider:
         return self.page.query_selector_all(selector)
 
     def click_element(self, selector):
-        self.perform_action_on_element(selector, lambda element: element.click())
+        self.execute_action(selector, lambda element: element.click())
 
     def double_click_element(self, selector):
-        self.perform_action_on_element(selector, lambda element: element.dblclick())
+        self.execute_action(selector, lambda element: element.dblclick())
 
     def fill_element(self, selector, text):
-        self.perform_action_on_element(selector, lambda element: element.fill(text))
+        self.execute_action(selector, lambda element: element.fill(text))
 
     def press_key(self, key):
         self.page.keyboard.press(key)
@@ -96,9 +84,7 @@ class BaseSpider:
         return self.page.screenshot(path)
 
     def upload_file(self, selector, file_path):
-        self.perform_action_on_element(
-            selector, lambda element: element.set_input_files(file_path)
-        )
+        self.execute_action(selector, lambda element: element.set_input_files(file_path))
 
     def execute_script(self, script) -> Any:
         """Execute a script on the page. The script can be a string of JavaScript code or a path to a JavaScript file."""
@@ -144,10 +130,3 @@ class BaseSpider:
 
     def wait_for_timeout(self, seconds):
         self.page.wait_for_timeout(seconds * 1000)
-
-    def close(self):
-        """Close the browser and stop the driver."""
-        try:
-            self.browser.close()
-        finally:
-            self._driver.stop()
