e5901b160a93f141845531705ad390f31b45577e
CJ <2771004336@qq.com>
2024-02-29 23:26:39 +0800

    ref:重构表结构

--- pages/boss/src/db.py
+++ pages/boss/src/db.py
@@ -2,7 +2,10 @@ import datetime
 
 import pytz
 from sqlalchemy import create_engine, Column, Integer, String, Index, DateTime, UniqueConstraint
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+from core.utils import logger
 
 _engine = create_engine('sqlite:///database.db')
 _Session = sessionmaker(bind=_engine)
@@ -14,18 +17,22 @@ class Boss(_Base):
     __tablename__ = 'boss'
 
     id = Column(Integer, primary_key=True)
-    company = Column(String, unique=True)
-    job_title = Column(String)
-    salary = Column(String)
-    address = Column(String)
-    category = Column(String)
-    link = Column(String)
+    company = Column(String)
+    job_title = Column(String, nullable=False)
+    min_salary = Column(Integer, nullable=False)
+    max_salary = Column(Integer, nullable=False)
+    address = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    path = Column(String, unique=True, nullable=False)
+    city = Column(String, nullable=False)
     creation_time = Column(DateTime, server_default=datetime.datetime.now(pytz.timezone('Asia/Shanghai')).isoformat())
 
     # 添加索引
     __table_args__ = (
-        UniqueConstraint('company'),
-        Index('idx_company_name', 'company'),
+        UniqueConstraint('path'),
+        Index('idx_path', 'path'),
+        Index('idx_address', 'address'),
+        Index('idx_city', 'city'),
         Index('idx_category', 'category')
     )
 
@@ -36,8 +43,12 @@ def setup():
 
 
 def insert(data):
-    _session.add(Boss(**data))
-    _session.commit()
+    try:
+        _session.add(Boss(**data))
+        _session.commit()
+    except SQLAlchemyError:
+        logger.warning(f"Error inserting: {data}")
+        _session.rollback()
 
 
 if __name__ == '__main__':
