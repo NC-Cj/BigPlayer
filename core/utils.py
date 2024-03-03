b0f8edb95a4f33d04be46d12cf2263a5c92c1dd0
CJ <2771004336@qq.com>
2024-03-03 19:48:29 +0800

    fix:修复loguru包使用bug

--- core/utils.py
+++ core/utils.py
@@ -1,16 +1,4 @@
-import inspect
-import sys
-
 from loguru import logger
-
-format_ = '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> ' \
-          '| <magenta>{process}</magenta>:<yellow>{thread}</yellow> ' \
-          '| <cyan>{extra[func_module]}</cyan>:<cyan>{extra[func_name]}</cyan>:<yellow>{extra[func_lineno]}</yellow> - <level>{message}</level>'
-logger.remove(0)
-logger.add(
-    sys.stderr,
-    format=format_
-)
 
 
 def validate_element_presence(nullable, element, selector):
@@ -27,18 +15,8 @@ def print_log(info=None):
         def inner(*args, **kwargs):
             result = fn(*args, **kwargs)
 
-            # 获取被装饰函数的信息
-            func_name = fn.__name__
-            func_module = inspect.getmodule(fn).__name__
-            try:
-                func_lineno = inspect.currentframe().f_back.f_lineno  # 获取调用位置（执行位置）的行号
-            except Exception:  # 防止异常情况，如在闭包或生成器中使用时可能获取不到行号
-                func_lineno = None
-
-            context = {"func_name": func_name, "func_module": func_module, "func_lineno": func_lineno}
-
-            log_message = f"{info}" if info else f"{func_name} done..."
-            logger.bind(**context).debug(log_message)
+            log_message = f"{info}" if info else f"{fn.__name__} done..."
+            logger.debug(log_message)
 
             return result
 
